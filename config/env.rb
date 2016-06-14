0f6750e60a94acdb71777c8cd6c0d3fde2b835cd
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-06-14 14:40:16 +0100

    use json config file

--- config/env.rb
+++ config/env.rb
@@ -1,7 +1,12 @@
 require 'bundler'
 Bundler.require
 
-require_relative '../config/clef'
+path = File.expand_path "../../", __FILE__
+PATH = path
+
+clef_configs = JSON.load File.open "#{path}/config/clef.json"
+CLEF_APP_ID     = clef_configs["CLEF_APP_ID"]
+CLEF_APP_SECRET = clef_configs["CLEF_APP_SECRET"]
 
 require_relative '../lib/clef'
 require_relative '../lib/user'
